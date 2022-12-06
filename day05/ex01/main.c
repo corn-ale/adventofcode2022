687ce5761e42fe4b26101973b67d7facf49ee12b
Kornel Konyari <kkonyari@1-A-10.42heilbronn.de>
2022-12-06 18:34:54 +0100

    finished ex01

--- day05/ex01/main.c
+++ day05/ex01/main.c
@@ -5,6 +5,16 @@
 #include <ctype.h>
 #include "get_next_line.h"
 
+
+
+/* ************************************************ */
+/* libft                                            */
+/*   |                                              */
+/*   |                                              */
+/*   v                                              */
+/*                                                  */
+/* ************************************************ */
+
 // size_t	ft_strlen(const char *s)
 // {
 // 	size_t	index;
@@ -98,14 +108,24 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (newstr);
 }
 
-/* libft                                            */
+
+
+/* ************************************************ */
+/*  main                                            */
+/*   |                                       ^      */
+/*   |                                       |      */
+/*   v                                       |      */
+/*                                         libft    */
 /* ************************************************ */
 
-int	main()
+
+
+int	fakemain()
 {
 	char	*line;
 	char	*trimmed;
 	char	**setup;
+	char	**arrayOfStacks;
 	int		i;
 	int		size;
 	int		fd;
@@ -114,7 +134,9 @@ int	main()
 	int		numOfCrates;
 
 
-	// malloc 2d array
+	// malloc for **setup here
+	// later on the input file will be read into **setup until
+	// the first empty line is found
 	i = 0;
 	fd = open("../input.txt", O_RDONLY);
 	if (fd < 0)
@@ -148,17 +170,20 @@ int	main()
 	setup[size] = NULL;
 
 
-	// read into 2d array
+	// read into **setup here
+	// had to use gnl with BUFFER_SIZE set to 1
 	fd2 = open("../input.txt", O_RDONLY);
 	if (fd2 < 0)
 	{
 		printf("error with file\n");
+		free(setup);
 		return (EXIT_FAILURE);
 	}
 	line = get_next_line(fd2);
 	if (!line)
 	{
 		printf("error: file is empty\n");
+		free(setup);
 		return (EXIT_FAILURE);
 	}
 	while (i < size)
@@ -167,10 +192,13 @@ int	main()
 		line = get_next_line(fd2);
 		i++;
 	}
+	free(line);
 	close(fd2);
 
 
 	// count number of stacks
+	// later this number will be used to malloc a 
+	// 2d array which will represent the stacks
 	numOfStacks = 0;
 	int l;
 	int m = 0;
@@ -191,6 +219,12 @@ int	main()
 	
 
 	// count number of crates
+	// the approach is to count the total num of crates in
+	// the input file, because this is the max number of
+	// crates that a single stack can have (in case all of the
+	// crates have been stacked into one single stack)
+	// this way i wont have to reallocate the stacks every time
+	// a crate is moved, just need to rewrite them with '\0'
 	numOfCrates = 0;
 	int n = 0;
 	int o = 0;
@@ -208,20 +242,246 @@ int	main()
 	printf("num of crates: %d\n", numOfCrates);
 
 
-	// init array of stacks
+	// init **arrayOfStacks and load the crates into them
+	//
+	// 1: malloc for **arrayOfStacks and
+	// 2: free already allocated stuff if malloc fails midway through
+	// 3: find last array before the NULL array in **setup and
+	// 4: iterate through it to find the positions of the numbers
+	//    which represent the crates
+	// 5: using the position of the numbers/crates, iterate throught
+	//    **setup UPWARDS, and load the crates into the correct place
+	//    in **arrayOfStacks
+	//
+	// part 1,
+	arrayOfStacks = malloc(sizeof(char *) * (numOfStacks + 1));
+	if (!arrayOfStacks)
+	{
+		printf("error with malloc\n");
+		for (int k = 0; setup[k]; k++)
+			free(setup[k]);
+		free(setup);
+		return (EXIT_FAILURE);
+	}
+	arrayOfStacks[numOfStacks] = NULL;
+	for (int p = 0; p < numOfStacks; p++)
+	{
+		arrayOfStacks[p] = calloc(1, sizeof(char) * (numOfCrates + 1));
+		if (arrayOfStacks[p] == NULL)
+		{									// part 2,
+			for (int z = 0; z <= p; z++)
+				free(arrayOfStacks[z]);
+			free(arrayOfStacks);
+			for (int k = 0; setup[k]; k++)
+				free(setup[k]);
+			free(setup);
+			printf("error with malloc\n");
+			return (EXIT_FAILURE);
+		}
+	}
+	// part 3,
+	l = 0;
+	m = 0;
+	int q = -1;
+	int	r = 0;
+	for (l = 0; setup[l + 1] != NULL; l++)
+		;
+	while (setup[l][m] != '\0')
+	{
+		while (!isdigit(setup[l][m]) && setup[l][m] != '\0')
+			m++;
+		if (isdigit(setup[l][m]))
+		{							// part 4, found the crate
+			q++;
+			r = 0;
+			for (int tmp = l - 1; tmp >= 0 && isalpha(setup[tmp][m]); tmp--)
+			// for (int tmp = l - 1; tmp >= 0; tmp--)
+			{						// part 5, loading crates into stacks
+				arrayOfStacks[q][r] = setup[tmp][m];
+				r++;
+			}
+			while (isdigit(setup[l][m]))
+				m++;
+		}
+	}
+	// printing the starting state of stacks
+	// for (int tmp = 0; arrayOfStacks[tmp]; tmp++)
+	// 	printf("stack %d: %s\n", tmp+1, arrayOfStacks[tmp]);
 
+
+	// read from input and execute the instructions
+	//
+	// 1: skip until there is an empty line in file
+	// 2: after new line read the instructions
+	// 3: get numbers from line
+	// 4: execute instructions
+	int fd3 = open("../input.txt", O_RDONLY);
+	if (fd3 < 0)
+	{
+		printf("error with file\n");
+		for (int k = 0; setup[k]; k++)
+			free(setup[k]);
+		free(setup);
+		for (int k = 0; arrayOfStacks[k]; k++)
+			free(arrayOfStacks[k]);
+		free(arrayOfStacks);
+		return (EXIT_FAILURE);
+	}
+	// part 1, skipping lines
+	char *instr = get_next_line(fd3);
+	if (!instr)
+	{
+		printf("error: empty file\n");
+		for (int k = 0; setup[k]; k++)
+			free(setup[k]);
+		free(setup);
+		for (int k = 0; arrayOfStacks[k]; k++)
+			free(arrayOfStacks[k]);
+		free(arrayOfStacks);
+		return (EXIT_FAILURE);
+	}
+	while (instr[0] != '\n')
+	{
+		free(instr);
+		instr = get_next_line(fd3);
+	}
+	free(instr);
+	// creating getNums used for part 3
+	instr = get_next_line(fd3);
+	int	tmp = 0;
+	int	nCount = 0;
+	int	digits = 0;
+	char **getNums = malloc(sizeof(char *) * 4);
+	int	nums[3];
+	if (!getNums)
+	{
+		printf("error with malloc\n");
+		for (int k = 0; setup[k]; k++)
+			free(setup[k]);
+		free(setup);
+		for (int k = 0; arrayOfStacks[k]; k++)
+			free(arrayOfStacks[k]);
+		free(arrayOfStacks);
+		return (EXIT_FAILURE);
+	}
+	getNums[3] = NULL;
+	for (int qq = 0; qq < 3; qq++)
+	{
+		getNums[qq] = calloc(1, sizeof(char) * 3);
+		if (getNums[qq] == NULL)
+		{
+			for (int k = 0; getNums[k]; k++)
+				free(getNums[k]);
+			free(getNums);
+			for (int k = 0; setup[k]; k++)
+				free(setup[k]);
+			free(setup);
+			for (int k = 0; arrayOfStacks[k]; k++)
+				free(arrayOfStacks[k]);
+			free(arrayOfStacks);
+		}
+	}
+	// part 2, read the instructions
+	char	Crate;
+	int		ww = 0;
+	int		yy = 0;
+	// for (int tmp = 0; arrayOfStacks[tmp]; tmp++)
+	// 	printf("stack %d: |%s|\n", tmp+1, arrayOfStacks[tmp]);
+	// printf("\n");
+	// for (int i = 0; i < 10; i++)
+	while (instr)
+	{
+		// printf("%s", instr);
+		// valid instructions here
+		nCount = 0;
+		tmp = 0;
+		while (instr[tmp])
+		{
+			if (isdigit(instr[tmp]))
+			{							// part 3, get the nums
+				digits = 0;
+				while (isdigit(instr[tmp]))
+				{
+					getNums[nCount][digits] = instr[tmp];
+					tmp++;
+					digits++;
+					nums[nCount] = atoi(getNums[nCount]);
+				}
+				nCount++;
+			}
+			tmp++;
+		}
+		// numbers are converted to ints and loaded into nums here
+		// printf("%s %s %s\n", getNums[0], getNums[1], getNums[2]);
+		// printf("%d %d %d\n", nums[0], nums[1], nums[2]);
+		for (int l = 0; l < 3; l++)
+			for (int k = 0; k < 2; k++)
+				getNums[l][k] = '\0';
+		
+		// part 4, execute instructions
+		while (nums[0] > 0)		// move nums[0] number of crates
+		{						
+			// go to stacks nums[1]
+			for (ww = 0; arrayOfStacks[nums[1] - 1][ww + 1] != '\0'; ww++)
+				;				// find last crate before null
+			Crate = arrayOfStacks[nums[1] - 1][ww];
+			// printf("[%c] ", Crate);					// take crate
+			arrayOfStacks[nums[1] - 1][ww] = '\0';
+			for (yy = 0; arrayOfStacks[nums[2] - 1][yy] != '\0'; yy++)
+				;
+			arrayOfStacks[nums[2] - 1][yy] = Crate;
+			nums[0]--;
+		}
+		// for (int tmp = 0; arrayOfStacks[tmp]; tmp++)
+		// 	printf("stack %d: %s\n", tmp+1, arrayOfStacks[tmp]);
+		// printf("\n");
+		
+
+		free(instr);
+		instr = get_next_line(fd3);
+	}
+
+	// printing the final state of stacks
+	printf("final state of stacks:\n");
+	for (int tmp = 0; arrayOfStacks[tmp]; tmp++)
+		printf("stack %d: %s\n", tmp+1, arrayOfStacks[tmp]);
 
 	
+	int le = 0;
+	printf("top crates at the end: ");
+	for (int tmp = 0; arrayOfStacks[tmp]; tmp++)
+	{
+		le = strlen(arrayOfStacks[tmp]);
+		printf("%c", arrayOfStacks[tmp][le - 1]);
+	}
+	printf("\n");
+	
+	close(fd3);
+
+
 	// print 2d array, then free 2d array
-	for (int j = 0; j < i; j++)
-		printf("%s", setup[j]);
+	// for (int j = 0; j < i; j++)
+	// 	printf("%s", setup[j]);
 	for (int k = 0; setup[k]; k++)
 		free(setup[k]);
 	free(setup);
+	for (int k = 0; arrayOfStacks[k]; k++)
+		free(arrayOfStacks[k]);
+	free(arrayOfStacks);
+	for (int k = 0; getNums[k]; k++)
+		free(getNums[k]);
+	free(getNums);
 
 
 	// trimmed = ft_strtrim(line, "\n");
 	// free(line);
 	// free(trimmed);
+	return (EXIT_SUCCESS);
+}
+
+int main()
+{
+	fakemain();
+	// system("leaks a.out");
 	return (EXIT_SUCCESS);
 }
